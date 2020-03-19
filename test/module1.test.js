cc1023d61d3f75d99c5c1639c623f28177371223
buddhini81 <buddhinisam@hotmail.com>
2020-03-19 12:29:26 +0530

    Test added for verifying the use of this keyword.

--- test/module1.test.js
+++ test/module1.test.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 const source = fs.readFileSync('js/shoppinggame.js', 'utf8');
 
+let playerDef;
 let prodNode;
 let defineProperties;
 let daysToExpireCall;
@@ -16,6 +17,12 @@ let fieldDeclInRatingCls = false;
 
 try {
     esprima.parseModule(source, {}, function (node) {
+        if (node.type == 'VariableDeclarator' &&
+            node.id.name == 'player' &&
+            node.init.type == 'ObjectExpression') {
+            playerDef = node;
+        }
+
         if (node.id && node.id.name === "Product" && node.type === "FunctionDeclaration") {
             prodNode = node;
         }
@@ -104,17 +111,67 @@ describe('Shopping Master game', function () {
 
         it('Should add three methods to the player object. @player-object-methods', function () {
 
+
+            const methods = playerDef && playerDef.init.properties.filter(element => element.value.type == 'FunctionExpression');
+
+            //console.log(methods);
+
             test.assert(shoppinggame.player &&
                 shoppinggame.player.getCurrentScore, "Have you added a method named `getCurrentScore` to the player object?");
-            test.assert(shoppinggame.player.getCurrentScore() == shoppinggame.player.score, "Have you returned the player's score from the method?");
-
             test.assert(shoppinggame.player.addPoints, "Have you added a method named `addPoints` to the player object?");
+            test.assert(shoppinggame.player.deductPoints, "Have you added a method named `deductPoints` to the player object?");
+
+            let thisUsedIngetCurrentScore = false;
+            let thisUsedInaddPoints = false;
+            let thisUsedIndeductPoints = false;
+
+            for (let i = 0; i < methods.length; i++) {
+
+                if (methods[i].key.name == 'getCurrentScore' &&
+                    methods[i].value.body.body[0].argument &&
+                    methods[i].value.body.body[0].argument.object &&
+                    methods[i].value.body.body[0].argument.object.type == 'ThisExpression') {
+                    thisUsedIngetCurrentScore = true;
+                }
+
+                if (methods[i].key.name == 'addPoints' &&
+                    methods[i].value.body.body[0].expression &&
+                    methods[i].value.body.body[0].expression.left.object &&
+                    methods[i].value.body.body[0].expression.left.object.type == 'ThisExpression' &&
+                    methods[i].value.body.body[0].expression.right.left.object &&
+                    methods[i].value.body.body[0].expression.right.left.object.type == 'ThisExpression') {
+                    thisUsedInaddPoints = true;
+                }
+
+                if (methods[i].key.name == 'deductPoints' &&
+                    methods[i].value.body.body[0].expression &&
+                    methods[i].value.body.body[0].expression.left.object &&
+                    methods[i].value.body.body[0].expression.left.object.type == 'ThisExpression' &&
+                    methods[i].value.body.body[0].expression.right.left.object &&
+                    methods[i].value.body.body[0].expression.right.left.object.type == 'ThisExpression') {
+                    thisUsedIndeductPoints = true;
+                }
+
+            }
+
+
+            console.log(thisUsedIngetCurrentScore+ " " + thisUsedInaddPoints+ " " +thisUsedIndeductPoints);
+
+            test.assert(thisUsedIngetCurrentScore &&
+                thisUsedInaddPoints &&
+                thisUsedIndeductPoints, "Have you used the `this` keyword for the `score` property within all three methods?");
+
+
+            test.assert(shoppinggame.player.getCurrentScore() == shoppinggame.player.score, "Have you returned the player's score from the method?");
             shoppinggame.player.addPoints(10);
             test.assert(shoppinggame.player.score == 10, "Have you added the points to the current score of the player?");
 
-            test.assert(shoppinggame.player.deductPoints, "Have you added a method named `deductPoints` to the player object?");
+
             shoppinggame.player.deductPoints(10);
             test.assert(shoppinggame.player.score == 0, "Have you subtracted the points from the current score of the player?");
+
+
+
         });
     });
 
@@ -286,8 +343,8 @@ describe('Shopping Master game', function () {
             //     objectCreateCall.right.arguments.object.name == 'Product' &&
             //     objectCreateCall.right.arguments.property &&
             //     objectCreateCall.right.arguments.property.name == 'prototype') ? true : false;
-            
-            test.assert(objectCreateCall , "Have you used Object.create(), passing Product.prototype as a parameter to link MagicProduct and Product?");
+
+            test.assert(objectCreateCall, "Have you used Object.create(), passing Product.prototype as a parameter to link MagicProduct and Product?");
 
         });
     });
@@ -296,16 +353,16 @@ describe('Shopping Master game', function () {
 
         it('Should define and export a class named Rating. @Rating-class', function () {
 
-            test.assert(fieldDeclInRatingCls == false , "Have you done a `Field declaration` in the class body? It is an experimental feature according to MDN docs and we suggest you remove it.");
+            test.assert(fieldDeclInRatingCls == false, "Have you done a `Field declaration` in the class body? It is an experimental feature according to MDN docs and we suggest you remove it.");
 
             test.assert(ratingClassDecl, "Have you defined a class named `Rating` using the `class` keyword and exported it?");
 
-                
+
             const classBody = ratingClassDecl.body.body;
 
             // let constructorIndex = -1;
             // let setterIndex = -1;
-            
+
             // if(classBody.length > 0 && classBody.length == 1) {
             //     constructorIndex = 0;
             //     setterIndex = 0;
@@ -316,9 +373,9 @@ describe('Shopping Master game', function () {
 
             let constructorDefined, setterDefined = false;
 
-            for(let i= 0; i < classBody.length; i++) {
+            for (let i = 0; i < classBody.length; i++) {
 
-                if(classBody[i] &&
+                if (classBody[i] &&
                     classBody[i].kind == 'constructor' &&
                     classBody[i].key.name == 'constructor' &&
                     classBody[i].value.type == 'FunctionExpression' &&
@@ -326,58 +383,58 @@ describe('Shopping Master game', function () {
                     classBody[i].value.body.body[0].expression.left.object.type == 'ThisExpression' &&
                     classBody[i].value.body.body[0].expression.left.property.name == 'rate') {
 
-                        constructorDefined = true; 
-                        break; 
-                    }
+                    constructorDefined = true;
+                    break;
+                }
             }
 
-            for(let i= 0; i < classBody.length; i++) {
+            for (let i = 0; i < classBody.length; i++) {
 
-                if(classBody[i] &&
-                        classBody[i].kind == 'set' &&
-                        classBody[i].key.name == 'rating' &&
-                        classBody[i].value.type == 'FunctionExpression') {
+                if (classBody[i] &&
+                    classBody[i].kind == 'set' &&
+                    classBody[i].key.name == 'rating' &&
+                    classBody[i].value.type == 'FunctionExpression') {
 
-                            setterDefined = true;
-                            break;
-                        }                    
+                    setterDefined = true;
+                    break;
+                }
             }
 
 
             test.assert(constructorDefined, "Have you added a constructor that initializes a property named `rate` to a default value?");
 
-            
+
             //console.log(setterDefined);
             test.assert(setterDefined, "Have you added a setter that sets the value of a property named `rate`?");
-            
+
             // let r = new shoppinggame.Rating();
             // const range1,range2,range3,range4 = false;
 
             const getRangeRate = (val) => {
-                let rangeRating = {range:"",rate:""};
+                let rangeRating = { range: "", rate: "" };
 
-               if(val > 1 && val <= 4) {
-                rangeRating.range = "> 1 && <= 4";
-                rangeRating.rate = "OK";
-               } else if(val >= 5 && val <= 7) {
-                rangeRating.range = ">= 5 && <= 7";
-                rangeRating.rate = "GOOD";
-               } else if(val > 7) {
-                rangeRating.range = "> 7";
-                rangeRating.rate = "EXCEPTIONAL";
-               } else {
-                rangeRating.range = "== 1";
-                rangeRating.rate = "BAD";
-               }
-               return rangeRating;
+                if (val > 1 && val <= 4) {
+                    rangeRating.range = "> 1 && <= 4";
+                    rangeRating.rate = "OK";
+                } else if (val >= 5 && val <= 7) {
+                    rangeRating.range = ">= 5 && <= 7";
+                    rangeRating.rate = "GOOD";
+                } else if (val > 7) {
+                    rangeRating.range = "> 7";
+                    rangeRating.rate = "EXCEPTIONAL";
+                } else {
+                    rangeRating.range = "== 1";
+                    rangeRating.rate = "BAD";
+                }
+                return rangeRating;
             };
 
-           
+
 
             let r = new shoppinggame.Rating();
             let rr;
-            
-            for(let i = 1; i <= 10; i++) {
+
+            for (let i = 1; i <= 10; i++) {
                 r.rating = i;
                 rr = getRangeRate(i);
 
@@ -385,9 +442,9 @@ describe('Shopping Master game', function () {
 
                 test.assert(r.rate != "" && r.rate == rr.rate, `Have you set this.rate to a value of ${rr.rate} for the value range: ${rr.range}?`);
             }
-             
-        });  
-    }); 
+
+        });
+    });
 
 });
 
